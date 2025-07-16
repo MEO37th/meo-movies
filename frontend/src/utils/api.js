55930390ec84964c8ddfee89f37117c5418c35da
MEO37TH <obuma.michael@gmail.com>
2025-07-17 00:10:06 +0100

    modified:   frontend/src/utils/api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://meo-movies.onrender.com",
+  baseURL: "https://meo-movies.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
